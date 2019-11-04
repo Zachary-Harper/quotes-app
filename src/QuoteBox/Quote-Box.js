1d3fa21443f45f3b23ae93356be30b419ac3ff34
Zachary Harper <zachary.harper402@gmail.com>
2019-11-04 12:40:41 -0600

    Created Quote Box component and linked current files

---
+++ src/QuoteBox/Quote-Box.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import App from "../App";
+class QuouteBox extends Component {
+    render() {
+        return (
+            <h1>AHJKMNHU</h1>
+        )
+    }
+}
+export default App;
